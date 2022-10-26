30cb65f61c6d94eb608b3c98f424dc906eb86017
M Odd <emad.mollaei@gmail.com>
2022-10-26 20:27:32 +0330

    checks all locations now

--- app/src/main/java/space/kheyrati/nanowatch/MyApplication.java
+++ app/src/main/java/space/kheyrati/nanowatch/MyApplication.java
@@ -11,18 +11,25 @@ public class MyApplication extends Application {
     public static CompanyResponseModel company;
 
     public static boolean locationValid() {
-        if (lastLocation == null || company == null || company.getLocation() == null) return false;
-        if (BuildConfig.DEBUG)
-            Log.e("MyApplication", "locationValid: " + isDistanceValid(lastLocation));
-        return isDistanceValid(lastLocation) < company.getLocation().get(0).getRadius();
+        if (lastLocation == null || company == null || company.getLocation() == null || company.getLocation().isEmpty()) return false;
+        double distance = Integer.MAX_VALUE;
+        double radius = Integer.MIN_VALUE;
+        for (int i = 0; i < company.getLocation().size(); i++) {
+            if(distanceOf(lastLocation, company.getLocation().get(i)) < distance){
+                distance = distanceOf(lastLocation, company.getLocation().get(i));
+            }
+            if(company.getLocation().get(i).getRadius() > radius)
+                radius = company.getLocation().get(i).getRadius();
+        }
+        return distance < radius;
     }
 
-    public static double isDistanceValid(Location point1) {
+    public static double distanceOf(Location point1, CompanyLocationResponseModel point2) {
         if(company == null || company.getLocation() == null || company.getLocation().isEmpty())
             return Integer.MAX_VALUE;
         double distance = Math.sqrt(
-                Math.pow(point1.getLatitude() - company.getLocation().get(0).getLat(), 2) +
-                        Math.pow(point1.getLongitude() - company.getLocation().get(0).getLon(), 2));
+                Math.pow(point1.getLatitude() - point2.getLat(), 2) +
+                        Math.pow(point1.getLongitude() - point2.getLon(), 2));
         return distance * 100000;
     }
 
